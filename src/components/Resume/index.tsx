cb6a311b7307f71c4f4e96645e1950ba643cbd27
Jennifer Durthaler <jennifer.durthaler@gmail.com>
2026-03-04 09:53:15 +0100

    Remove export PDF button

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -17,7 +17,7 @@ export function Resume() {
       <div className="flex items-center justify-between mb-4">
         <div className="flex items-center gap-2">
           <PdfDownload />
-          <ExportPdfContent />
+          {/* <ExportPdfContent /> */}
         </div>
         <div className="flex items-center gap-2">
           <LanguageToggle />
